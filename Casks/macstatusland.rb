4d0c3531937cd6e780e75f64c4f29a0ed766fc3c
wangxiaorui <wangxiaorui.njk@jtmail.founder.com>
2026-06-02 14:44:40 +0800

    chore: update MacStatusLand to v2.0.0

--- Casks/macstatusland.rb
+++ Casks/macstatusland.rb
@@ -1,6 +1,6 @@
 cask "macstatusland" do
-  version "1.0.0"
-  sha256 "17c2884ab074d13e5aa77043362abbf741a0ddd88d5ea786de28aee65a0d71ea"
+  version "2.0.0"
+  sha256 "ae6324b7f43441e319c9595f9a4fcba196baacf59d4569b9fd4d620daf66a661"
 
   url "https://github.com/xiaoRui278/mac-status-land/releases/download/v#{version}/MacStatusLand-v#{version}.dmg"
   name "MacStatusLand"
