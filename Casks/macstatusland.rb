63f09ade460563c28ba440133f1079eb0a045c18
wangxiaorui <wangxiaorui.njk@jtmail.founder.com>
2026-06-02 15:02:17 +0800

    chore: update MacStatusLand SHA256

--- Casks/macstatusland.rb
+++ Casks/macstatusland.rb
@@ -1,6 +1,6 @@
 cask "macstatusland" do
   version "2.0.0"
-  sha256 "3f792aa9357114a693664456ad8a77e79d159d66fe84b1bea138606556b98bb9"
+  sha256 "783491952bf53e8777a15e0037ab7878e079b5dba077bfaa3bd91d630b989fb7"
 
   url "https://github.com/xiaoRui278/mac-status-land/releases/download/v#{version}/MacStatusLand-v#{version}.dmg"
   name "MacStatusLand"
